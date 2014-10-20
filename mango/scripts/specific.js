6fe0c31743e29a668f0bd635dea751bfc692e1a0
Anne L'Hôte <anne.lhote@gmail.com>
2014-10-20 16:21:58 +0200

    Template : Update CSS and change page template

--- mango/scripts/specific.js
+++ mango/scripts/specific.js
@@ -1,11 +1,15 @@
 $(document).ready(
-	function(){
-		var iSid = $('#sid').val();
-		// Coop - Introduction, Coop - Jeu catégorisation Peur/Colère, Coop - Jeu Preferences, Coop - Jeu Public Good, Coop - Questionnaire Anxiété, Coop - Questionnaire Autisme, Coop - Questionnaire Bien Etre, Coop - Questionnaire Relations Sociales, Coop - Questionnaire Traits d'histoire de vie
-		var aCoopSurveys = ['232525', '867323', '948898', '876211', '778262', '271959', '776855', '984752', '718757'];
+	function() {
+		
+		var iSid			= $('#sid').val();
+		// Coop - Introduction Session 01, Coop - Introduction Session 02, Coop - Jeu Catégorisation Peur/Colère, Coop - Jeu Dictator, Coop - Jeu Preferences, Coop - Jeu Public Good, Coop - Jeu Trust, Coop - Questionnaire Anxiété, Coop - Questionnaire Autisme, Coop - Questionnaire Bien Etre, Coop - Questionnaire Relations Sociales, Coop - Questionnaire Traits d'histoire de vie
+		var aCoopSurveys	= ['232525', '233138', '867323', '336985', '948898', '876211', '914824', '778262', '271959', '776855', '984752', '718757'];
+		// GC - Jeu Discounting, GC - Jeu Labyrinthe, GC - Jeu Meta Cognition, GC - Jeu Motivation Sociale, GC - Questionnaire Auto-efficacité, GC - Questionnaire Bien-être, GC - Questionnaire Echelle de tenacité, GC - Questionnaire Facteurs de réussite, GC - Questionnaire Prévoyance, GC - Questionnaire Réalisme, GC - Questionnaire V2
+		var aGcSurveys		= ['537887', '969373', '639163', '982922', '499881', '937529', '448264', '899228', '519196', '158295', '356627'];
 		// For survey 718757, Coop - Questionnaire Traits d'histoire de vie, hide Question Help
-		if(aCoopSurveys.indexOf(iSid) != -1) {
+		if((aCoopSurveys.indexOf(iSid) != -1) || (aGcSurveys.indexOf(iSid) != -1)) {
 			$('.questionhelp').hide();
+			$('#movesubmitbtn').text('Suivant');
 			$('.question input.numeric').keyup(
 				function(event) {
 					// If a number is entered, hide the questionhelp message
@@ -17,9 +21,6 @@ $(document).ready(
 					}
 				}
 			);
-		}
-		if(aCoopSurveys.indexOf(iSid) != -1) {
-			$('#movesubmitbtn .ui-button-text').text('Suivant');
 		}
 	}
 );
